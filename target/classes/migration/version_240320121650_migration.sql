2c7dee4e86a8819480c3deb382d1d550f645b5b6
sumitsuri <sumitbharti800@gmail.com>
2024-08-07 08:06:36 +0530

    Delete target directory

--- target/classes/migration/version_240320121650_migration.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `upload_type_registry` (`id`, `upload_type`, `CREATED_BY`, `MODIFIED_BY`, `created_on`, `updated_on`) VALUES (2, 'data_loader', 13, 13, '2021-03-23 11:46:26', '2021-03-23 11:46:27');
-INSERT INTO `upload_type_registry` (`id`, `upload_type`, `CREATED_BY`, `MODIFIED_BY`, `created_on`, `updated_on`) VALUES (1, 'bulk_upload', 13, 13, '2021-03-23 11:46:26', '2021-03-23 11:46:27');
